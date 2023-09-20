87756b8823f971a9c19867a2c9cc204b3042d130
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-20 12:18:36 +0100

    add db schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
+/* table for animals */
 CREATE TABLE animals (
-    name varchar(100)
+    id int PRIMARY KEY,
+    name varchar(225),
+    date_of_birth: date,
+    escape_attempts: integer
+    neutered: boolean
+    weight_kg: decimal
 );
