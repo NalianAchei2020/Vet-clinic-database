b3a35256d7293ac300c7630282aaca7070c19595
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-21 11:38:02 +0100

    Update data.sql
    insert data into species table

--- data.sql
+++ data.sql
@@ -21,7 +21,10 @@ VALUES
    INSERT INTO owners (name, age)
    VALUES ('Sam Smith', 34),
    ('Jennifer Orwell ', 19),
-   ('Bob', 45)
+   ('Bob', 45),
    ('Melody Pond', 77),
    ('Dean Winchester', 14),
-   ('Jodie Whittaker', 38)
+   ('Jodie Whittaker', 38);
+
+-- inserting data into the species table
+   INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
