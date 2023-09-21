4c2724370476a2d5439bed130d38183a9e8a027c
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-21 13:28:58 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -43,15 +43,15 @@ WHERE name = 'Agumon';
 
 UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
-WHERE name  IN ('Gabumon ', 'Pikachu')
+WHERE name  IN ('Gabumon', 'Pikachu')
 
 UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
 WHERE name  IN ('Devimon', 'Plantmon')
 
 UPDATE animals
-SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond ')
-WHERE name  IN ('Charmander ', 'Squirtle', 'Blossom')
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name  IN ('Charmander', 'Squirtle', 'Blossom')
 
 UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
