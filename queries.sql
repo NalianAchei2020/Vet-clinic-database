63328f71b0c709f76c895ec6bb2f4ca50b206a92
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-21 10:11:56 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -37,3 +37,6 @@ WHERE date_of_birth > '2022-01-01';
 SELECT * FROM animals;
 
 SAVEPOINT my_savepoint;
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
